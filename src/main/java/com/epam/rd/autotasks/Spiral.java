71378dce45573b156cbb1080cdfe446e25ddc462
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-12-17 20:10:12 +0100

    spiral_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/Spiral.java
+++ src/main/java/com/epam/rd/autotasks/Spiral.java
@@ -1,39 +1,36 @@
 package com.epam.rd.autotasks;
 
 class Spiral {
-    static int[][] spiral(int rows, int columns) {
 
-        int[][] arr = new int[rows][columns];
+  static int[][] spiral(int rows, int columns) {
 
-        int num = 1;
-        int max = rows * columns;
+    int[][] arr = new int[rows][columns];
 
-        int rmin = 0, rmax = rows - 1;
-        int cmin = 0, cmax = columns - 1;
+    int num = 1;
+    int max = rows * columns;
 
-        while (num <= max) {
-            // проход влево по верхней строке, инкремент столбцов
-            for (int i = cmin; num <= max && i <= cmax; i++) {
-                arr[rmin][i] = num++;
-            }
-            rmin++;
-            // проход вниз по правому краю, инкремент строк
-            for (int i = rmin; num <= max && i <= rmax; i++) {
-                arr[i][cmax] = num++;
-            }
-            cmax--;
-            // проход вправо по нижней строке, декремент столбцов
-            for (int i = cmax; num <= max && i >= cmin; i--) {
-                arr[rmax][i] = num++;
-            }
-            rmax--;
-            // проход вверх по левому краю, декремент строк
-            for (int i = rmax; num <= max && i >= rmin; i--) {
-                arr[i][cmin] = num++;
-            }
-            cmin++;
-        }
+    int rmin = 0, rmax = rows - 1;
+    int cmin = 0, cmax = columns - 1;
 
-        return arr;
+    while (num <= max) {
+      for (int i = cmin; num <= max && i <= cmax; i++) {
+        arr[rmin][i] = num++;
+      }
+      rmin++;
+      for (int i = rmin; num <= max && i <= rmax; i++) {
+        arr[i][cmax] = num++;
+      }
+      cmax--;
+      for (int i = cmax; num <= max && i >= cmin; i--) {
+        arr[rmax][i] = num++;
+      }
+      rmax--;
+      for (int i = rmax; num <= max && i >= rmin; i--) {
+        arr[i][cmin] = num++;
+      }
+      cmin++;
     }
+
+    return arr;
+  }
 }
